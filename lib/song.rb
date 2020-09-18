8588d0cbd35026049341324bb51d591c7ed38b9b
billyott <willrichott@gmail.com>
2020-09-17 20:30:59 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -10,4 +13,67 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    return song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    return song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    return song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    song_attributes = filename.split(" - ")
+    artist = song_attributes[0]
+    song_name = song_attributes[1].gsub(".mp3", "")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song_attributes = filename.split(" - ")
+    artist = song_attributes[0]
+    song_name = song_attributes[1].gsub(".mp3", "")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist
+    song.save
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+
 end
+
+
+# alison = Song.create_by_name("Alison")
+# binding.pry
